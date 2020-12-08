6aa850a81cabdea1d3b6ff5d208f21397c6852ac
RNorris6 <ryan.norris@patelhs.org>
2020-12-08 17:19:32 +0000

    finished the first step of the assignment in order to print 25

--- Main.java
+++ Main.java
@@ -1,5 +1,9 @@
 class Main {
   public static void main(String[] args) {
-    
+    //objects are created from classes
+
+    Sum s = new Sum(12,13);
+
+    System.out.println(s.sumOf());
   }   
 }
---
+++ Sum.java
@@ -0,0 +1,20 @@
+public class Sum
+{
+  //attributes--AKA variables
+  private int num1;
+  private int num2;
+
+  //this method adds two numbers
+  public int sumOf()
+  {
+    return num1 + num2;
+  }
+
+  //constructor-- assigns values to my private variables
+  public Sum(int number1, int number2)
+  {
+    num1 = number1;
+    num2 = number2;
+  }
+
+}
